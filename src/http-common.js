1717952f6237ca7afcadb64c1d09a81b3d076813
HiddenHistory <110185906+HiddenHistory@users.noreply.github.com>
2023-05-02 00:12:13 -0400

    oh
    jo

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default axios.create({
     //URL to the general database url to connect to it and parse / modify its contents. URL can be changed to access the modification section. You need to do this because axios will not be allowed to access the database directly through the frontend website, which isn't a url allowed to access the database. Doing this basically lets axios route its request through a url that's allowed to give it.
-    baseURL: "https://haia.herokuapp.com",
+    baseURL: "https://haia.herokuapp.com/api/v1/entries",
 
     headers: {
         "Content-Tye": "application/json"
